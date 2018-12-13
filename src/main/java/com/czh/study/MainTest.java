9ac3d1460636ce75447b5d85f79c8c6a3fda22a1
caiz.henghao <123456>
2018-12-13 19:22:27 +0800

    提交结构调整代码

---
+++ src/main/java/com/czh/study/MainTest.java
@@ -0,0 +1,13 @@
+package com.czh.study;
+
+/**
+ * @Author: cai.zhenghao
+ * @Description:
+ * @Date: Created in 2018/12/12  4:02 PM
+ * @Modified By:
+ */
+public class MainTest {
+    public static void main(String[] args){
+        System.out.println("test");
+    }
+}
