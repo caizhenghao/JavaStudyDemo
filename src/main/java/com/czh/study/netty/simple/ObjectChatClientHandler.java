83fb20cc48c1819db50bb07c3b19cde6f9ef0819
caiz.henghao <123456>
2018-12-17 19:32:04 +0800

    实现简单的netty 通讯demo

---
+++ src/main/java/com/czh/study/netty/simple/ObjectChatClientHandler.java
@@ -0,0 +1,42 @@
+package com.czh.study.netty.simple;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * @Author: cai.zhenghao
+ * @Description:
+ * @Date: Created in 2018/12/17  5:42 PM
+ * @Modified By:
+ */
+public class ObjectChatClientHandler extends ChannelInboundHandlerAdapter{
+
+    public ObjectChatClientHandler(){
+
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) {
+        // Send the first message if this handler is a client-side handler.
+        ctx.writeAndFlush("\"Hello ,I am client");
+    }
+
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+//        super.channelRead(ctx, msg);
+        System.out.println("Receive msg from server:" + msg);
+        ctx.write(msg);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
---
+++ src/main/java/com/czh/study/netty/simple/ObjectChatServer.java
@@ -0,0 +1,71 @@
+package com.czh.study.netty.simple;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.serialization.ClassResolvers;
+import io.netty.handler.codec.serialization.ObjectDecoder;
+import io.netty.handler.codec.serialization.ObjectEncoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.SelfSignedCertificate;
+
+
+/**
+ * @Author: cai.zhenghao
+ * @Description: 简单的收发消息demo，参考了github上https://github.com/netty/netty 的例子
+ * @Date: Created in 2018/12/17  5:49 PM
+ * @Modified By:
+ */
+public class ObjectChatServer {
+    static final boolean SSL = System.getProperty("ssl") != null;
+    static final int PORT = Integer.parseInt(System.getProperty("port", "8007"));
+
+    public static void main(String[] args) throws Exception {
+        //configure ssl
+        final SslContext sslCtx;
+        if (SSL) {
+            SelfSignedCertificate ssc = new SelfSignedCertificate();
+            sslCtx = SslContextBuilder.forServer(ssc.certificate(), ssc.privateKey()).build();
+        } else {
+            sslCtx = null;
+        }
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            ChannelPipeline p = socketChannel.pipeline();
+                            if (sslCtx != null) {
+                                p.addLast(sslCtx.newHandler(socketChannel.alloc()));
+                            }
+
+                            p.addLast(new ObjectEncoder(),
+                                    new ObjectDecoder(ClassResolvers.cacheDisabled(null)),
+                                    new ObjectChatServerHandler());
+                        }
+                    });
+
+            // Bind and start to accept incoming connections.
+            b.bind(PORT).sync().channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
+
+    }
+
+}
