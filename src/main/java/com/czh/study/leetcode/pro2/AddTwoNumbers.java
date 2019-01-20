a263046ba2e356f4fd88d18a0c0190f1f278aee0
caizhenghao <1083962846@qq.com>
2019-01-21 00:53:15 +0800

    提交第二道leetcode题目的代码

--- src/main/java/com/czh/study/leetcode/pro2/AddTwoNumbers.java
+++ src/main/java/com/czh/study/leetcode/pro2/AddTwoNumbers.java
@@ -58,4 +58,25 @@ public class AddTwoNumbers {
             val = x;
         }
     }
+
+    //最优答案更为简洁，实际运行效率相当，暂时不明白为什么有除法和求余也能比我快
+//    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//        ListNode dummyHead = new ListNode(0);
+//        ListNode p = l1, q = l2, curr = dummyHead;
+//        int carry = 0;
+//        while (p != null || q != null) {
+//            int x = (p != null) ? p.val : 0;
+//            int y = (q != null) ? q.val : 0;
+//            int sum = carry + x + y;
+//            carry = sum / 10;
+//            curr.next = new ListNode(sum % 10);
+//            curr = curr.next;
+//            if (p != null) p = p.next;
+//            if (q != null) q = q.next;
+//        }
+//        if (carry > 0) {
+//            curr.next = new ListNode(carry);
+//        }
+//        return dummyHead.next;
+//    }
 }
